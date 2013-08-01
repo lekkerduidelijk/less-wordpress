4c669b27308b36b30083f3a1b188b802c73b0abd
Rutger Laurman <lekkerduidelijk@gmail.com>
2013-08-01 14:41:13 +0200

    Update functions.php
    Fix indenting

--- functions.php
+++ functions.php
@@ -161,6 +161,6 @@ add_action('admin_init','lwp_add_gf_access');
  */
 function is_blog () {
   global  $post;
-	$posttype = get_post_type($post );
-	return ( ((is_archive()) || (is_author()) || (is_category()) || (is_home()) || (is_single()) || (is_tag())) && ( $posttype == 'post')  ) ? true : false ;
+  $posttype = get_post_type($post );
+  return ( ((is_archive()) || (is_author()) || (is_category()) || (is_home()) || (is_single()) || (is_tag())) && ( $posttype == 'post')  ) ? true : false ;
 }
