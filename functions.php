c1432bedab1feb570b1111eb60da654ad7b6a10b
Rutger Laurman <lekkerduidelijk@gmail.com>
2013-08-01 14:40:46 +0200

    Update functions.php
    Adding is_blog method

--- functions.php
+++ functions.php
@@ -154,3 +154,13 @@ function lwp_add_gf_access(){
   $role->add_cap('gform_full_access');
 }
 add_action('admin_init','lwp_add_gf_access');
+
+/**
+ * Check if post is a blog post
+ * https://gist.github.com/wesbos/1189639
+ */
+function is_blog () {
+  global  $post;
+	$posttype = get_post_type($post );
+	return ( ((is_archive()) || (is_author()) || (is_category()) || (is_home()) || (is_single()) || (is_tag())) && ( $posttype == 'post')  ) ? true : false ;
+}
