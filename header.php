c186eb2a1461de1ff9b3488692f7c1c26bb62ccb
Rutger Laurman <lekkerduidelijk@gmail.com>
2014-04-05 18:08:10 +0200

    Update header.php
    Add JS and SVG checks without Modernizr, adds/removes HTML classes accordingly

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
  * @subpackage LESS_Wordpress
  */
 ?><!doctype html>
+<html class="no-js">
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <title><?php wp_title('&laquo;') ?></title>
@@ -12,13 +13,19 @@
 // See: http://lrdk.nl/fn
 ?>
 <meta name="viewport" content="width=device-width">
+<script>
+  // Check for javascript support
+  (function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement);
+
+  // Check for SVG support
+  (function(H){if(!!("createElementNS"in document&&document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect)){H.className+=" svg"}else{H.className+=" no-svg"}})(document.documentElement);
+</script>
 <link rel="stylesheet" type="text/css" href="<?php echo versioned_resource('/css/style.css') ?>">
 <?php wp_head(); ?>
 <!--[if lt IE 9]>
   <script src="<?php echo versioned_resource('/js/oldbrowsers.js') ?>"></script>
 <![endif]-->
 <body <?php body_class(); ?>>
-<!--[if lte IE 7]><iframe src="http://www.browserupgrade.info/ie6-upgrade/?lang=en&gc=true" frameborder="no" style="height: 81px; width: 100%; border: none;"></iframe><![endif]-->
 <div id="container">
   <header role="banner">
     <figure id="logo"><a href="<?php echo get_option('home'); ?>"><img src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="<?php bloginfo('name'); ?>"></a></figure>
